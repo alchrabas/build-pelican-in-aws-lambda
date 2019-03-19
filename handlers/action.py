671a613c9cdb3eb0a7e38750dbb767c54340456f
alchrabas <alchrabas@exeris.org>
2019-03-19 23:41:04 +0100

    Fix upload to S3, extract params to variables and pass through envvars

--- handlers/action.py
+++ handlers/action.py
@@ -1,5 +1,3 @@
-'''<HANDLER DESCRIPTION>'''
-
 import json
 import logging
 import os
@@ -16,14 +14,14 @@ logging.root.setLevel(logging.getLevelName(log_level))
 _logger = logging.getLogger(__name__)
 
 MAIN_DIR = "/tmp"
-BLOG_DIR = "/tmp/blog"
+BLOG_DIR = MAIN_DIR + "/blog"
 
 client = boto3.client('lambda')
-s3 = boto3.resource('s3')
+s3 = boto3.client('s3')
 
 
 def http_rebuild_blog(event, context):
-    client.invoke(FunctionName="build-pelican-in-lambda-dev-async_rebuild_blog_from_git",
+    client.invoke(FunctionName=os.environ["LAMBDA_REBUILD_ASYNC"],
                   InvocationType='Event')
 
 
@@ -38,21 +36,23 @@ def build_blog_from_git(main_dir, blog_dir):
     if os.path.isdir(blog_dir):
         shutil.rmtree(blog_dir)  # just fetch from the repo (including submodule dependencies)
     stdout, stderr = git.exec_command('clone', '--recurse-submodules',
-                                      'https://github.com/alchrabas/blog.git', blog_dir, cwd=main_dir)
+                                      os.environ["URL_TO_GIT_REPO_HTTPS"], blog_dir, cwd=main_dir)
     os.chdir(blog_dir)
     settings = read_settings("pelicanconf.py")
     pelican = Pelican(settings)
     pelican.run()
 
-    upload_recursively(blog_dir + "/output", "chrabasz.cz")
+    upload_recursively(blog_dir + "/output", os.environ["BUCKET_NAME"])
 
     _logger.info('Git stdout: {}, stderr: {}'.format(stdout.decode("utf-8"), stderr.decode("utf-8")))
 
 
-def upload_recursively(path, bucketname):
+def upload_recursively(path, bucket_name):
     for root, dirs, files in os.walk(path):
-        for file in files:
-            s3.upload_file(os.path.join(root, file), bucketname, file)
+        for filename in files:
+            local_path = os.path.join(root, filename)
+            relative_path = os.path.relpath(local_path, path)
+            s3.upload_file(local_path, bucket_name, relative_path)
 
 
 if __name__ == "__main__":
